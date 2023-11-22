a3f97371fb36f128e2edbcf62c6607473c893e33
masoud allahyari <masoud.allahyari@t-systems.com>
2023-11-22 20:40:45 +0100

    nothig

--- launchers/dpf-selector/build.gradle.kts
+++ launchers/dpf-selector/build.gradle.kts
@@ -19,10 +19,38 @@ plugins {
 }
 
 dependencies {
-    implementation(project(":extensions:common:api:api-observability"))
-    api(project(":core:data-plane-selector:data-plane-selector-core"))
-    api(project(":extensions:data-plane-selector:data-plane-selector-api"))
-    api(project(":extensions:data-plane-selector:data-plane-selector-client"))
+    implementation(project(":core:control-plane:control-plane-core"))
+    implementation(project(":data-protocols:dsp"))
+    implementation(project(":extensions:common:auth:auth-tokenbased"))
+    // implementation(project(":extensions:common:vault:vault-filesystem"))
+     implementation(project(":extensions:common:vault:vault-hashicorp"))
+    implementation(project(":extensions:common:configuration:configuration-filesystem"))
+    implementation(project(":extensions:common:iam:iam-mock"))
+//    implementation(project(":extensions:common:iam:oauth2:oauth2-daps"))
+    implementation(project(":extensions:control-plane:api:management-api"))
+    // Embedded DPF
+    implementation(project(":extensions:control-plane:transfer:transfer-data-plane"))
+    implementation(project(":extensions:data-plane-selector:data-plane-selector-client"))
+    implementation(project(":spi:data-plane-selector:data-plane-selector-spi"))
+    implementation(project(":extensions:control-plane:api:control-plane-api-client"))
+    implementation(project(":core:control-plane:control-plane-aggregate-services"))
+    implementation(project(":extensions:control-plane:api:control-plane-api"))
+    implementation(project(":core:data-plane-selector:data-plane-selector-core"))
+    implementation(project(":core:data-plane:data-plane-core"))
+    implementation(project(":spi:control-plane:transfer-data-plane-spi"))
+    //implementation(project(":core:data-plane:data-plane-core"))
+
+    //implementation(project(":extensions:data-plane:data-plane-api"))
+    implementation(project(":extensions:common:metrics:micrometer-core"))
+    implementation(project(":extensions:data-plane:data-plane-http"))
+    implementation("org.eclipse.edc:data-plane-aws-s3:0.4.1")
+    //implementation("org.eclipse.edc:provision-aws-s3:0.3.1")
+    // implementation(project(":extensions:data-plane:data-plane-kafka"))
+    testImplementation(libs.junit.jupiter.api)
+    testRuntimeOnly(libs.junit.jupiter.engine)
+    //  implementation(project(":extensions:data-plane:data-plane-client"))
+    implementation(project(":extensions:data-plane-selector:data-plane-selector-api"))
+
 }
 
 application {
